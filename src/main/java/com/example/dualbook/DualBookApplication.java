ddb0ed5d79ad6905a4b44e84f64a474a489c234c
sinaerfani <sinaerfani66@gmail.com>
2025-10-06 19:30:32 +0330

    Initial commit - DualBook backend setup

---
+++ src/main/java/com/example/dualbook/DualBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.dualbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DualBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DualBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dualbook/DualBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dualbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DualBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
